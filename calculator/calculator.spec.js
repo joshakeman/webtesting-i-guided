bac82ea942b535557144ecb07f904a4ccef6b591
Josh Akeman <joshakeman@gmail.com>
2019-05-13 13:10:11 -0400

    tests for add function working

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,15 @@
-const { add } = require('./calculator.js');
+const calculator = require('./calculator.js');
 
-// test away!
+
+describe('calculator.js', () => {
+    describe('add() method', () => {
+
+        it('should return the sume of two numbers', () => {
+
+            expect(calculator.add(2,2)).toBe(4)
+            expect(calculator.add(2,1)).toBe(3)
+            expect(calculator.add(2,3)).toBe(5)
+
+        })
+    })
+})
